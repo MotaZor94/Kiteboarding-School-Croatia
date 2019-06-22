ccdf4a29889775906f3a05ca602e36c96148e361
Petr Stoklas <petrstoklas@gmail.com>
2019-06-22 23:07:22 +0200

    app bar functionality

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -8,46 +8,115 @@ import Logo from '../../images/icons/logo.png'
 import KitesurfIcon from '../../images/icons/kitesurf.svg'
 import AboutusIcon from '../../images/icons/about-us.svg'
 
-const AppBar = () => (
-  <div className="app-bar-main-panel">
+import LocationColorIcon from '../../images/icons/location-color.svg'
+import WeatherColorIcon from '../../images/icons/cloud-color.svg'
+import KitesurfColorIcon from '../../images/icons/kitesurf-color.svg'
+
+class AppBar extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selected: {
+        location: false,
+        weather: false,
+        home: false,
+        rental: false,
+        contactus: false
+      }
+    }
+
+    this.handleContactusClick - this.handleContactusClick.bind()
+    this.handleWeatherClick - this.handleWeatherClick.bind()
+    this.handleHomeClick - this.handleHomeClick.bind()
+    this.handleRentalClick - this.handleRentalClick.bind()
+    this.handleContactusClick - this.handleContactusClick.bind()
+  }
+
+
+  handleLocationClick() {
+    this.setState({ 
+      selected: { 
+        location: true
+      }
+    })
+  }
+  handleWeatherClick() {
+    this.setState({ 
+      selected: { 
+        weather: true
+      }
+    })
+  }
+  handleHomeClick() {
+    this.setState({ 
+      selected: { 
+        home: true
+      }
+    })
+  }
+  handleRentalClick() {
+    this.setState({ 
+      selected: { 
+        rental: true
+      }
+    })
+  }
+  handleContactusClick() {
+    this.setState({ 
+      selected: { 
+        contactus: true
+      }
+    })
+  }
+  // handleClick(type) {
+  //   console.log(this.state.selected[type])
+  //   this.setState({ selected: {
+  //     [type]: true 
+  //     }
+  //   })
+  // }
+
+  render() {
+    const { selected: { location, weather, home, rental, contactus} } = this.state
+    return (
+      <div className="app-bar-main-panel">
     <Link to="/location">
-      <div className="app-bar-location">
-        <LocationIcon />
+      <div className="app-bar-location" onClick={() => this.handleLocationClick()}>
+        {location ? <LocationColorIcon /> : <LocationIcon />}
         <p className="app-bar-desc location">LOCATION</p>
       </div>
     </Link>
 
     <Link to="/weather">
-    <div className="app-bar-weather">
-    <WeatherIcon />
+    <div className="app-bar-weather" onClick={() => this.handleWeatherClick()}>
+    {weather ? <WeatherColorIcon /> : <WeatherIcon />}
         <p className="app-bar-desc location">WEATHER</p>
     </div>
     </Link>
 
-    <Link to="/home">
+    <Link to="/home" onClick={() => this.handleHomeClick()}>
     <div className="app-bar-home">
     <img alt="aenona"src={Logo}></img>
     </div>
     </Link>
 
     <Link to="/rental">
-    <div className="app-bar-rental">
-    <KitesurfIcon />
+    <div className="app-bar-rental" onClick={() => this.handleRentalClick()}>
+    {rental ? <KitesurfColorIcon /> : <KitesurfIcon />}
         <p className="app-bar-desc location">RENTAL</p>
     </div>
     </Link>
 
     <Link to="/aboutus">
     <div className="app-bar-about-us">
-    <AboutusIcon />
+    <AboutusIcon /> 
         <p className="app-bar-desc location">ABOUT US</p>
     </div>
-    </Link>
-    
-    
-      
-    
+    </Link>  
   </div>
-);
+    )
+  }
+}
 
 export default AppBar;
