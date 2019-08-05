8606c257cf3ccd349d5fe58aa16078bd1f62075b
Petr Stoklas <petrstoklas@gmail.com>
2019-08-05 20:45:33 +0200

    Cleaning 2.

--- src/components/pages/main/main.js
+++ src/components/pages/main/main.js
@@ -18,35 +18,15 @@ export default function main({ location }) {
       <h3>Push your kiteboarding skills with our astonishing equipment</h3>
       <h3><strong>Ready to surf?</strong></h3>
       <div className="main-button-wrapper">
-      <Link  className="icon-link" to="/rental"><button className="rental-button">RENT NOW</button></Link>
-      <Link className="icon-link" to="/weather"><button className="weather-button">WEATHER REPORT</button></Link>
+        <Link  className="icon-link" to="/rental"><button className="rental-button">RENT NOW</button></Link>
+        <Link className="icon-link" to="/weather"><button className="weather-button">WEATHER REPORT</button></Link>
       </div>
       <div className="Image-Wrap">
-      <ImageComp
-       image={NinCroatia}
-       imageheading = "Discover Nin,Croatia"
-       imagedesc = "Croatian Pre-Romanesque Catholic church originating from the 9th century"
-       />
-       <ImageComp
-       image={NinCroatia}
-       imageheading = "Discover Nin,Croatia"
-       imagedesc = "Croatian Pre-Romanesque Catholic church originating from the 9th century"
-       />
-       <ImageComp
-       image={NinCroatia}
-       imageheading = "Discover Nin,Croatia"
-       imagedesc = "Croatian Pre-Romanesque Catholic church originating from the 9th century"
-       />
-       <ImageComp
-       image={NinCroatia}
-       imageheading = "Discover Nin,Croatia"
-       imagedesc = "Croatian Pre-Romanesque Catholic church originating from the 9th century"
-       />
-       <ImageComp
-       image={NinCroatia}
-       imageheading = "Discover Nin,Croatia"
-       imagedesc = "Croatian Pre-Romanesque Catholic church originating from the 9th century"
-       />
+        <ImageComp
+          image={NinCroatia}
+          imageheading = "Discover Nin,Croatia"
+          imagedesc = "Croatian Pre-Romanesque Catholic church originating from the 9th century"
+        />
        </div>
       
     </div>
